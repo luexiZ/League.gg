8700b7297e87a4ef3f438dedf9f68a621bdb3f79
BT_1E10_30 <BT_1E10_30@bths.edu>
2022-05-24 10:57:17 -0400

    League classwork

--- src/GUIController.java
+++ src/GUIController.java
@@ -1,5 +1,6 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -16,16 +17,27 @@ public class GUIController implements ActionListener
     private JTextField userEntryField;
     private JLabel Top1_5;
     private JLabel Top6_10;
-    private Player results;
+    private JPanel bottomPanel;
+    private Player player;
     private LeagueOfLegendsClient client;
+    private CardLayout card;
+    private JLabel pictureLabel1;
+    private JLabel pictureLabel2;
+    private JLabel pictureLabel3;
+
 
     public GUIController()
     {
         userEntryField = new JTextField(9);
         Top1_5 = new JLabel();
         Top6_10 = new JLabel();
-        results = null;
+        bottomPanel = new JPanel();
+        card = new CardLayout();
+        player = null;
         client = new LeagueOfLegendsClient();
+        pictureLabel1 = new JLabel();
+        pictureLabel2 = new JLabel();
+        pictureLabel3 = new JLabel();
         screenGUI();
 
     }
@@ -33,7 +45,7 @@ public class GUIController implements ActionListener
     public void screenGUI()
     {
         JFrame frame = new JFrame("League.GG");
-        frame.setPreferredSize( new Dimension(500, 220));
+        frame.setPreferredSize( new Dimension(480, 220));
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 
 
@@ -55,24 +67,25 @@ public class GUIController implements ActionListener
         titleLabel.setForeground(Color.green);
         title.add(titleLabel);
 
-
-        JPanel topPlayers = new JPanel();
+        JPanel topPlayerPanel = new JPanel();
         GridLayout layout = new GridLayout(1,2);
-        topPlayers.setLayout(layout);
+        topPlayerPanel.setLayout(layout);
+        bottomPanel.setLayout(card);
         layout.setHgap(10);
-        Top1_5 = new JLabel("<html>1.  <br> 2. Quntao Zheng <br> 3. Qihong , <br> 4. player4 <br> 5. player5 <html>");
+        Top1_5 = new JLabel("Top 5 ");
         Top1_5.setHorizontalAlignment(SwingConstants.CENTER);
         Top6_10 = new JLabel("<html>6. <br> player6 <br> 7. player7 <br> 8. player8 <br> 9. player9 <br> 10. player10 <br> <html>");
         Top6_10.setHorizontalAlignment(SwingConstants.CENTER);
 
 
-        topPlayers.add(Top1_5);
-        topPlayers.add(Top6_10);
+        topPlayerPanel.add(Top1_5);
+        topPlayerPanel.add(Top6_10);
+        bottomPanel.add(topPlayerPanel);
 
 
         frame.add(searchPanel, BorderLayout.NORTH);
         frame.add(title, BorderLayout.CENTER);
-        frame.add(topPlayers, BorderLayout.SOUTH);
+        frame.add(bottomPanel, BorderLayout.SOUTH);
 
 
         submit.addActionListener(this);
@@ -98,6 +111,7 @@ public class GUIController implements ActionListener
         else if (text.equals("Clear"))
         {
             userEntryField.setText("");
+            card.next(bottomPanel);
 
 
         }
@@ -105,28 +119,25 @@ public class GUIController implements ActionListener
 
     public void displayInfo(String Username) // call this method when submit is click ; add PARAMETERS !
     {
-        results = client.getPlayer(Username);
-        ArrayList<Champion> championList = results.getMostPlayed();
+        JPanel infoPanel = new JPanel();
+        player = client.getPlayer(Username);
+        ArrayList<Champion> championList = player.getMostPlayed();
 
-        Top1_5.setText("<html> Solo Rank: " + results.getSoloRank() + " <br> Solo Win Rate: " +  winRate(results.getSoloWinRate()) + "<br>Flex Rank: " + results.getFlexRank()+ "<br>Flex WinRate: "+ winRate(results.getFlexWinRate()) + "<br> <html>");
-        Champion champ1 = results.getMostPlayed().get(0);
-        Champion champ2 = results.getMostPlayed().get(1);
-        Champion champ3 = results.getMostPlayed().get(2);
-        Top6_10.setText("MostPlayed:" + champ1.getName() + "<br>" + champ2.getName() + "<br>" + champ3.getName() +"<br>");
+        JLabel rankLabel = new JLabel("<html> Solo Rank: " + player.getSoloRank() + "      " +"                                   <br> Solo Win Rate: " +  winRate(player.getSoloWinRate()) + "<br>Flex Rank: " + player.getFlexRank()+ "<br>Flex WinRate: "+ winRate(player.getFlexWinRate()) + "<br> <html>");
+        Champion champ1 = player.getMostPlayed().get(0);
+        Champion champ2 = player.getMostPlayed().get(1);
+        Champion champ3 = player.getMostPlayed().get(2);
+        JLabel championsLabel = new JLabel("<html>Most Played: <br>" + champ1.getName() + " " + champ1.getPoints() + "<br>" + champ2.getName() + " " + champ2.getPoints() + "<br>" + champ3.getName() + " " + champ3.getPoints() +"<br> <html>");
+        postImage(player.getMostPlayed());
+        infoPanel.add(rankLabel, BorderLayout.WEST);
+        infoPanel.add(championsLabel, BorderLayout.EAST);
+        infoPanel.add(pictureLabel1);
+        infoPanel.add(pictureLabel2);
+        infoPanel.add(pictureLabel3);
+        bottomPanel.add(infoPanel);
 
-        BufferedImage bufImg = null;
-        Image tmp = null;
-        try {
-            URL url = new URL("https:" + champ1.getPictureURL());
-            bufImg = ImageIO.read(url);
-            tmp = bufImg.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        assert false;
-        Top6_10.setIcon(new ImageIcon(tmp));
+        card.next(bottomPanel);
+
 
     }
 
@@ -134,6 +145,39 @@ public class GUIController implements ActionListener
         DecimalFormat df = new DecimalFormat("###.##");
         return df.format(winRate * 100)+"%";
     }
+
+    public void postImage(ArrayList<Champion> champions)
+    {
+        for(int i = 0; i < champions.size(); i++)
+        {
+            BufferedImage bufImg = null;
+            Image tmp = null;
+            try {
+                URL url = new URL(champions.get(i).getPictureURL());
+                bufImg = ImageIO.read(url);
+                tmp = bufImg.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            assert false;
+            if(i == 0){
+                pictureLabel1.setIcon(new ImageIcon(tmp));
+            }
+            else if(i == 1){
+                pictureLabel2.setIcon(new ImageIcon(tmp));
+            }
+            else{
+                pictureLabel3.setIcon(new ImageIcon(tmp));
+            }
+        }
+    }
+
+//    public void topPlayer(){
+//        ArrayList<String> topPlayer = client.parseTopPlayers();
+//        for(int i = 0; i  topPlayer.size())
+//    }
 
 
 
--- src/LeagueOfLegendsClient.java
+++ src/LeagueOfLegendsClient.java
@@ -13,20 +13,22 @@ public class LeagueOfLegendsClient {
     private String baseUrl;
 
     public LeagueOfLegendsClient(){
-        APIkey = "RGAPI-4cf8c2a5-ef4d-42de-9075-e22a7384bca0";
+        APIkey = "RGAPI-f2783ad4-c65b-45a3-b77e-c7c646fb0d52";
         baseUrl = "https://na1.api.riotgames.com/lol";
     }
 
     public String getID(String name){
         name = fixName(name);
         String endPoint = "/summoner/v4/summoners/by-name/" + name;
-        String url = baseUrl + endPoint + "?api_key=" + APIkey;
+        // String url = baseUrl + endPoint + "?api_key=" + APIkey;
+        String url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=RGAPI-f2783ad4-c65b-45a3-b77e-c7c646fb0d52";
         JSONObject jsonObject = new JSONObject(makeAPICall(url));
         return jsonObject.getString("id");
     }
 
 
-    public ArrayList<Champion> parseJSONChampion(String summonerID){
+    public ArrayList<Champion> parseJSONChampion(String name){
+        String summonerID = getID(name);
         String endPoint = "/champion-mastery/v4/champion-masteries/by-summoner/" + summonerID;
         String url = baseUrl + endPoint + "?api_key=" + APIkey;
         String response = makeAPICall(url);
@@ -36,28 +38,31 @@ public class LeagueOfLegendsClient {
             JSONObject jsonObject = jsonArray.getJSONObject(i);
             int id = jsonObject.getInt("championId");
             int points = jsonObject.getInt("championPointsSinceLastLevel");
-            String name = "";
+            String championName = "";
             String pictureURL = "";
 
             JSONObject jsonChamp = new JSONObject(makeAPICall("http://ddragon.leagueoflegends.com/cdn/12.9.1/data/en_US/champion.json"));
             JSONObject data = jsonChamp.getJSONObject("data");
-            Iterator x = data.keys();
-            JSONArray jsonArray1 = new JSONArray();
-            while (x.hasNext()){
-                String key = (String)x.next();
-                jsonArray.put(data.get(key));
-            }
-            for(int j = 0; j < jsonArray1.length(); j++){
-                if(jsonArray1.getJSONObject(j).getInt("key") == id){
-                    name = jsonArray1.getJSONObject(j).getString("id");
-                    pictureURL = "http://ddragon.leagueoflegends.com/cdn/12.9.1/img/champion/" + name + ".png";
+            Iterator<String> keys = data.keys();
+
+            while(keys.hasNext()) {
+                String key = keys.next();
+                if (data.get(key) instanceof JSONObject) {
+                    JSONObject character = data.getJSONObject(key);
+                    int idInList = character.getInt("key");
+                    if(idInList == id){
+                        championName = character.getString("id");
+                        pictureURL = "http://ddragon.leagueoflegends.com/cdn/12.9.1/img/champion/" + championName + ".png";
+                    }
+
                 }
             }
-            Champion champion = new Champion(name, pictureURL, points);
+            Champion champion = new Champion(championName, pictureURL, points);
             list.add(champion);
         }
         return list;
     }
+
 
     public Player getPlayer(String name){
         String summonerID = getID(name);
@@ -69,8 +74,20 @@ public class LeagueOfLegendsClient {
         double soloWinRate;
         double flexWinRate;
         JSONArray jsonArray = new JSONArray(makeAPICall(url));
-        JSONObject solo = jsonArray.getJSONObject(1);
-        JSONObject flex = jsonArray.getJSONObject(0);
+        JSONObject solo = null;
+        JSONObject flex = null;
+        for(int i = 0; i < 3; i++)
+        {
+            if(jsonArray.getJSONObject(i).getString("queueType").equals("RANKED_SOLO_5x5"))
+            {
+                solo = jsonArray.getJSONObject(i);
+            }
+            if(jsonArray.getJSONObject(i).getString("queueType").equals("RANKED_FLEX_SR"))
+            {
+                flex = jsonArray.getJSONObject(i);
+            }
+        }
+
         soloRank = solo.getString("tier") + " " + solo.getString("rank") + " " + solo.getInt("leaguePoints");
         flexRank = flex.getString("tier") + " " + flex.getString("rank") + " " + flex.getInt("leaguePoints");
         soloWinRate = (double)solo.getInt("wins") / (solo.getInt("wins") + solo.getInt("losses"));
@@ -106,9 +123,36 @@ public class LeagueOfLegendsClient {
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
+
             return null;
         }
     }
+
+    public ArrayList<String> parseTopPlayers() {
+        String endPoint = "/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5";
+        String url = baseUrl + endPoint + "?api_key=" + APIkey;
+        String response = makeAPICall(url);
+
+        ArrayList<String> topPlayers = new ArrayList<>();
+        JSONObject jsonObject = new JSONObject(response);
+        JSONArray players = jsonObject.getJSONArray("entries");
+        JSONObject player = null;
+        int highestIdx = 0;
+        for (int i = 0; i < 10; i++) {
+            JSONObject highest = players.getJSONObject(0);
+            for (int j = 1; j < players.length(); j++) {
+                player = players.getJSONObject(j);
+                if (player.getInt("leaguePoints") > highest.getInt("leaguePoints")) {
+                    highest = player;
+                    highestIdx = j;
+                }
+            }
+            topPlayers.add(highest.getString("summonerName"));
+            players.remove(highestIdx);
+        }
+        return topPlayers;
+    }
+
 
 
 }
